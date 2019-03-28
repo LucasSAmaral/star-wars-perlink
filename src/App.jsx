ffa5386b42443643bc6466318aad6efee404b79f
Lucas <lucassantosamaral@hotmail.com>
2019-03-28 00:20:54 -0300

    Filtrando nomes dos filmes da API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.scss';
 
 class App extends Component {
@@ -6,8 +7,22 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      select: ''
+      select: '',
+      films: [],
+      people: []
     }
+  }
+
+  renderData(e) {
+    axios.get(`https://swapi.co/api/films/`)
+      .then(response => {
+        this.setState({films: response.data.results})
+        let resultFilms = this.state.films.map(film=>{return film.title});
+        console.log(resultFilms.filter(resultFilm => resultFilm.toLowerCase().includes(e)));
+      })
+      .catch(function (error){
+        console.log(error);
+      });
   }
 
   render() {
@@ -15,12 +30,12 @@ class App extends Component {
       <div className="App">
         <h1>Star Wars</h1>
         
-        <input type="text" placeholder="Type here" name="search" id="search"/>
+        <input onKeyUp={(e)=>this.renderData(e.target.value)} type="text" placeholder="Type here" name="search" id="search"/>
 
         <select value={this.state.select} onChange={(e)=>this.setState({select: e.target.value})} name="selector" id="select">
           <option value="n/a" defaultValue>Select an option</option>
-          <option value="Character">Character</option>
-          <option value="Film">Film</option>
+          <option value="people">People</option>
+          <option value="films">Films</option>
         </select>
       </div>
     );
