71b257ba71bbac8132664d1c8058e58365aa26e9
Lucas <lucassantosamaral@hotmail.com>
2019-03-30 17:39:03 -0300

    Feita a pagina de filmes

--- src/pages/Film.jsx
+++ src/pages/Film.jsx
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
+import Logo from '../components/Logo';
+import axios from 'axios';
 
 class Film extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            films: [],
+            newDate: ''
+        }
+    }
+
+    componentDidMount() {
+        this.renderFilms();
+    }
+
+    renderFilms() {
+        let filmNumber = window.location.pathname.split('/')[2];
+        axios.get(`https://swapi.co/api/films/${filmNumber}/?format=json`)
+            .then(response => {
+                let responseFilms = response.data;
+                this.setState({films: responseFilms});
+                let date = responseFilms.release_date;
+                let newDate = date.split('-').reverse().join('/');
+                this.setState({newDate: newDate});
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     render(){
         return(
-        <p>Film</p>
+            <div className="container__App">
+                <div className="container__logo">
+                    <Logo />
+                </div>
+
+                <div className="container__films">
+                    <h1>{this.state.films.title}</h1>
+
+                    <div className="container__films--description">
+                        <p><span>Film director:</span> {this.state.films.director}</p>
+                        <p><span>Episode number:</span> {this.state.films.episode_id}</p>
+                        <p><span>Release date:</span> {this.state.newDate}</p>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
