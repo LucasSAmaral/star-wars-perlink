1a9bbb8ff47a3a4046a8f8aad271e7aea42725ad
Lucas <lucassantosamaral@hotmail.com>
2019-03-30 13:02:27 -0300

    Ajuste na requisicao de pessoas

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,25 @@ class App extends Component {
 
         Promise.all(peopleFilm)
           .then(responses => {
-            this.setState({ personFilm: responses });
+            let peopleArray = [];
+            responses.forEach((element,index)=>{
+              if (element === "A New Hope") {
+                peopleArray[index] = [element, 1];
+              } else if (element === "The Empire Strikes Back") {
+                peopleArray[index] = [element, 2];
+              } else if (element === "Return of the Jedi") {
+                peopleArray[index] = [element, 3];
+              } else if (element === "The Phantom Menace") {
+                peopleArray[index] = [element, 4];
+              } else if (element === "Attack of the Clones") {
+                peopleArray[index] = [element, 5];
+              } else if (element === "Revenge of the Sith") {
+                peopleArray[index] = [element, 6];
+              } else if (element === "The Force Awakens") {
+                peopleArray[index] = [element, 7];
+              }
+            });
+            this.setState({ personFilm: peopleArray });
             return console.log(this.state.personFilm);
           })
 
@@ -114,8 +132,8 @@ class App extends Component {
 
         <div className={`container__result ${this.state.select !== 'people' ? 'display-none' : this.state.personFilm.length === 0 ? 'display-none' : ''}`}>
 
-          {this.state.personFilm.map(movie => {
-            return <Links key={movie} path="/film" text={movie} />
+          {this.state.personFilm.map(film => {
+            return <Links key={film[1]} path={`path/${film[1]}`} text={film[0]} />
           })}
 
         </div>
