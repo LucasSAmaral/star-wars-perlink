19bea47158e1c6a059607e8496785c4ed939b7d2
Lucas Santos Do Amaral <lucas.samaral@BRRJOBITAMM1932.local>
2019-05-03 10:34:36 -0300

    Alterando algumas variaveis de let pra const

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Logo from './components/Logo';
-import Links from './components/Links';
-import InputText from './components/InputText';
-import SelectSearch from './components/SelectSearch';
-import ButtonRouter from './components/ButtonRouter';
-import './scss/Style.scss';
-
+import React, { Component } from "react";
+import axios from "axios";
+import Logo from "./components/Logo";
+import Links from "./components/Links";
+import InputText from "./components/InputText";
+import SelectSearch from "./components/SelectSearch";
+import ButtonRouter from "./components/ButtonRouter";
+import "./scss/Style.scss";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      select: '',
+      select: "",
       films: [],
       personFilm: []
-    }
+    };
   }
 
   clearInput() {
-    let inputText = document.getElementById('search');
-    inputText.value = '';
+    const inputText = document.getElementById("search");
+    inputText.value = "";
     inputText.focus();
     this.clearSearch();
   }
@@ -32,122 +30,161 @@ class App extends Component {
   }
 
   renderFilms(e) {
-    // this.setState({ personFilm: [] });
     let lowerCaseFilm = e.toLowerCase();
-    axios.get(`https://swapi.co/api/films/`)
+    axios
+      .get(`https://swapi.co/api/films/`)
       .then(response => {
-        let responseFilms = response.data.results;
-        let resultFilms = responseFilms.map(film => { return film.title });
-        let movieArray = [];
-        resultFilms.forEach((element,index) => {
-          if(index === 0) {
-            movieArray[index] = [element,1];
+        const responseFilms = response.data.results;
+        const resultFilms = responseFilms.map(film => {
+          return film.title;
+        });
+        const movieArray = [];
+        resultFilms.forEach((element, index) => {
+          if (index === 0) {
+            movieArray[index] = [element, 1];
           } else if (index === 1) {
-            movieArray[index] = [element,5];
+            movieArray[index] = [element, 5];
           } else if (index === 2) {
-            movieArray[index] = [element,4];
+            movieArray[index] = [element, 4];
           } else if (index === 3) {
-            movieArray[index] = [element,6];
+            movieArray[index] = [element, 6];
           } else if (index === 4) {
-            movieArray[index] = [element,3];
+            movieArray[index] = [element, 3];
           } else if (index === 5) {
-            movieArray[index] = [element,2];
+            movieArray[index] = [element, 2];
           } else if (index === 6) {
-            movieArray[index] = [element,7];
-          }  
+            movieArray[index] = [element, 7];
+          }
         });
 
-        this.setState({ films: movieArray.filter(movie => movie[0].toLowerCase().includes(lowerCaseFilm)) });
+        this.setState({
+          films: movieArray.filter(movie =>
+            movie[0].toLowerCase().includes(lowerCaseFilm)
+          )
+        });
       })
-      .catch(function (error) {
+      .catch(function(error) {
         console.log(error);
       });
   }
 
   renderPeople(e) {
-    // this.setState({ films: [] });
     let lowerCasePeople = e.toLowerCase();
-    axios.get(`https://swapi.co/api/people/`)
+    axios
+      .get(`https://swapi.co/api/people/`)
       .then(response => {
-        let responsePeople = response.data.results;
-        let resultPeople = responsePeople.filter(person => person.name.toLowerCase().includes(lowerCasePeople));
-        let peopleFilms = resultPeople[0].films;
-        let peopleFilm = peopleFilms.map(film => {
-          return (
-            axios.get(film)
-              .then(response => {
-                let peopleMovies = response.data.title;
-                return peopleMovies;
-              })
-              .catch(function (error) {
-                console.log(error);
-              })
-          )
+        const responsePeople = response.data.results;
+        let resultPeople = responsePeople.filter(person =>
+          person.name.toLowerCase().includes(lowerCasePeople)
+        );
+        const peopleFilms = resultPeople[0].films;
+        const peopleFilm = peopleFilms.map(film => {
+          return axios
+            .get(film)
+            .then(response => {
+              const peopleMovies = response.data.title;
+              return peopleMovies;
+            })
+            .catch(function(error) {
+              console.log(error);
+            });
         });
 
-        Promise.all(peopleFilm)
-          .then(responses => {
-            let peopleArray = [];
-            responses.forEach((element,index)=>{
-              if (element === "A New Hope") {
-                peopleArray[index] = [element, 1];
-              } else if (element === "The Empire Strikes Back") {
-                peopleArray[index] = [element, 2];
-              } else if (element === "Return of the Jedi") {
-                peopleArray[index] = [element, 3];
-              } else if (element === "The Phantom Menace") {
-                peopleArray[index] = [element, 4];
-              } else if (element === "Attack of the Clones") {
-                peopleArray[index] = [element, 5];
-              } else if (element === "Revenge of the Sith") {
-                peopleArray[index] = [element, 6];
-              } else if (element === "The Force Awakens") {
-                peopleArray[index] = [element, 7];
-              }
-            });
-            this.setState({ personFilm: peopleArray });
-            return this.state.personFilm;
-          })
+        Promise.all(peopleFilm).then(responses => {
+          const peopleArray = [];
+          responses.forEach((element, index) => {
+            if (element === "A New Hope") {
+              peopleArray[index] = [element, 1];
+            } else if (element === "The Empire Strikes Back") {
+              peopleArray[index] = [element, 2];
+            } else if (element === "Return of the Jedi") {
+              peopleArray[index] = [element, 3];
+            } else if (element === "The Phantom Menace") {
+              peopleArray[index] = [element, 4];
+            } else if (element === "Attack of the Clones") {
+              peopleArray[index] = [element, 5];
+            } else if (element === "Revenge of the Sith") {
+              peopleArray[index] = [element, 6];
+            } else if (element === "The Force Awakens") {
+              peopleArray[index] = [element, 7];
+            }
+          });
+          this.setState({ personFilm: peopleArray });
+          return this.state.personFilm;
+        });
       })
-      .catch(function (error) {
-        console.log(error)
+      .catch(function(error) {
+        console.log(error);
       });
   }
 
   render() {
     return (
       <div className="container__App">
-
         <div className="container__logo">
           <Logo />
         </div>
 
         <div className="container__search">
-          <InputText searchText={this.state.select === 'films' ? (e) => this.renderFilms(e.target.value) : (e) => this.renderPeople(e.target.value)} placeholderText={this.state.select === 'films' ? 'Type film' : this.state.select === 'people' ? 'Type person' : 'Select an option first'}/>
-          <SelectSearch stateValue={this.state.select} changeState={(e) => {this.setState({ select: e.target.value });this.clearInput();}}/>
+          <InputText
+            searchText={
+              this.state.select === "films"
+                ? e => this.renderFilms(e.target.value)
+                : e => this.renderPeople(e.target.value)
+            }
+            placeholderText={
+              this.state.select === "films"
+                ? "Type film"
+                : this.state.select === "people"
+                ? "Type person"
+                : "Select an option first"
+            }
+          />
+          <SelectSearch
+            stateValue={this.state.select}
+            changeState={e => {
+              this.setState({ select: e.target.value });
+              this.clearInput();
+            }}
+          />
         </div>
 
-        <div className={`container__result ${this.state.select !== 'films' ? 'display-none' : this.state.films.length === 0 ? 'display-none' : ''}`}>
-
+        <div
+          className={`container__result ${
+            this.state.select !== "films"
+              ? "display-none"
+              : this.state.films.length === 0
+              ? "display-none"
+              : ""
+          }`}
+        >
           {this.state.films.map(film => {
-            return <Links key={film[1]} path={`/film/${film[1]}`} text={film[0]} />
+            return (
+              <Links key={film[1]} path={`/film/${film[1]}`} text={film[0]} />
+            );
           })}
-
         </div>
 
-        <div className={`container__result ${this.state.select !== 'people' ? 'display-none' : this.state.personFilm.length === 0 ? 'display-none' : ''}`}>
-
+        <div
+          className={`container__result ${
+            this.state.select !== "people"
+              ? "display-none"
+              : this.state.personFilm.length === 0
+              ? "display-none"
+              : ""
+          }`}
+        >
           {this.state.personFilm.map(film => {
-            return <Links key={film[1]} path={`/film/${film[1]}`} text={film[0]} />
+            return (
+              <Links key={film[1]} path={`/film/${film[1]}`} text={film[0]} />
+            );
           })}
-
         </div>
 
         <div className="container__buttons">
           <ButtonRouter path="/aboutme" text="About Me" />
           <ButtonRouter path="/abouttheapp" text="About The App" />
         </div>
-
       </div>
     );
   }
--- src/pages/Film.jsx
+++ src/pages/Film.jsx
@@ -1,62 +1,71 @@
-import React, { Component } from 'react';
-import Logo from '../components/Logo';
-import ButtonRouter from '../components/ButtonRouter';
-import axios from 'axios';
+import React, { Component } from "react";
+import Logo from "../components/Logo";
+import ButtonRouter from "../components/ButtonRouter";
+import axios from "axios";
 
 class Film extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      films: [],
+      newDate: ""
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            films: [],
-            newDate: ''
-        }
-    }
+  componentDidMount() {
+    this.renderFilms();
+  }
 
-    componentDidMount() {
-        this.renderFilms();
-    }
+  renderFilms() {
+    const filmNumber = window.location.pathname.split("/")[2];
+    axios
+      .get(`https://swapi.co/api/films/${filmNumber}/?format=json`)
+      .then(response => {
+        const responseFilms = response.data;
+        this.setState({ films: responseFilms });
+        const date = responseFilms.release_date;
+        const newDate = date
+          .split("-")
+          .reverse()
+          .join("/");
+        this.setState({ newDate: newDate });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-    renderFilms() {
-        let filmNumber = window.location.pathname.split('/')[2];
-        axios.get(`https://swapi.co/api/films/${filmNumber}/?format=json`)
-            .then(response => {
-                let responseFilms = response.data;
-                this.setState({films: responseFilms});
-                let date = responseFilms.release_date;
-                let newDate = date.split('-').reverse().join('/');
-                this.setState({newDate: newDate});
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
+  render() {
+    return (
+      <div className="container__App">
+        <div className="container__logo">
+          <Logo />
+        </div>
 
-    render(){
-        return(
-            <div className="container__App">
-                <div className="container__logo">
-                    <Logo />
-                </div>
+        <div className="container__films">
+          <h1>{this.state.films.title}</h1>
 
-                <div className="container__films">
-                    <h1>{this.state.films.title}</h1>
+          <div className="container__films--description">
+            <p>
+              <span>Film director:</span> {this.state.films.director}
+            </p>
+            <p>
+              <span>Episode number:</span> {this.state.films.episode_id}
+            </p>
+            <p>
+              <span>Release date:</span> {this.state.newDate}
+            </p>
+          </div>
+        </div>
 
-                    <div className="container__films--description">
-                        <p><span>Film director:</span> {this.state.films.director}</p>
-                        <p><span>Episode number:</span> {this.state.films.episode_id}</p>
-                        <p><span>Release date:</span> {this.state.newDate}</p>
-                    </div>
-                </div>
-
-                <div className="container__buttons">
-                        <ButtonRouter path="/" text="Home" />
-                        <ButtonRouter path="/aboutme" text="About Me" />
-                        <ButtonRouter path="/abouttheapp" text="About The App" />
-                </div>
-            </div>
-        )
-    }
+        <div className="container__buttons">
+          <ButtonRouter path="/" text="Home" />
+          <ButtonRouter path="/aboutme" text="About Me" />
+          <ButtonRouter path="/abouttheapp" text="About The App" />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Film
+export default Film;
