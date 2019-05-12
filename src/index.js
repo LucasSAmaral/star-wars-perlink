80cd280130015af8ff7f4ecf598d0edb1c28816f
Lucas <lucassantosamaral@hotmail.com>
2019-05-12 17:32:22 -0300

    Indentando sass e index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Film from './pages/Film';
-import AboutMe from './pages/AboutMe';
-import AboutTheApp from './pages/AboutTheApp';
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Film from "./pages/Film";
+import AboutMe from "./pages/AboutMe";
+import AboutTheApp from "./pages/AboutTheApp";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
-<BrowserRouter>
+  <BrowserRouter>
     <Switch>
-        <Route path="/" exact={true} component={App} />
-        <Route path="/film/:id" component={Film} />
-        <Route path="/aboutme" component={AboutMe}/>
-        <Route path="/abouttheapp" component={AboutTheApp} />
+      <Route path="/" exact={true} component={App} />
+      <Route path="/film/:id" component={Film} />
+      <Route path="/aboutme" component={AboutMe} />
+      <Route path="/abouttheapp" component={AboutTheApp} />
     </Switch>
-</BrowserRouter>
-, document.getElementById('root'));
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
